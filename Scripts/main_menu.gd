ffbb6c119663580d68423c3e2be290ff4ce486a5
decadentpig <jordandevisser@gmail.com>
2025-08-02 10:38:51 +0530

    Added main menu and global variables

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var new_game_button = get_node("New_Game_Button")
+@onready var challenge_mode_button = get_node("Challenge_Mode_Button")
+
+
+func _on_new_game_button_pressed() -> void:
+	GlobalVariables.tutorial_on = true
+	get_tree().change_scene_to_file("res://scene.tscn")
+
+func _on_challenge_mode_button_pressed() -> void:
+	GlobalVariables.tutorial_on = false
+	get_tree().change_scene_to_file("res://scene.tscn")
