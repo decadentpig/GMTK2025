60085a01cf69597fd014469e31e601b70bd9f712
decadentpig <jordandevisser@gmail.com>
2025-08-02 18:14:16 +0530

    Set background music to loop

--- Autoloads/music_player.gd
+++ Autoloads/music_player.gd
@@ -1,7 +1,17 @@
 extends AudioStreamPlayer2D
 
 @onready var audio_bg_track = preload("res://Sounds/ClockWork (BG music).mp3")
+@onready var audio_test = preload("res://Sounds/Factory Complete.wav")
+
+func _ready():
+	connect('finished', Callable(self, '_on_finished'))
+
+func _on_finished():
+	play_background_music()
 
 func play_background_music():
 	stream = audio_bg_track
 	play()
+
+func stop_background_music():
+	stop()
