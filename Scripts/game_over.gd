6b58329f99c608313042796eb20d2ceb1335e077
Harrison Blee <harryblee@hotmail.com>
2025-08-03 23:55:28 +1000

    Add game over screen

---
+++ Scripts/game_over.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var new_game_button = get_node("New_Game_Button")
+@onready var loops_label = get_node("Loops_Label")
+
+func _ready():
+	MusicPlayer.play_menu_music()
+	loops_label.text = "Loops: " + str(Stats.previous_game_loops)
+
+func _on_new_game_button_pressed() -> void:
+	GlobalVariables.tutorial_on = true
+	get_tree().change_scene_to_file("res://scene.tscn")
